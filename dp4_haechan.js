fc35f3054303b1ba05752e697faf1144a6d26d06
AnHaechan <mike000529@gmail.com>
2020-06-02 23:56:30 +0900

    img href and margin column

--- dp4_haechan.js
+++ dp4_haechan.js
@@ -1,20 +1,17 @@
-var s1 = {name: "blue hawaii", pic:"empty", abs:"15%", taste: "Watch and see the hawaii", ingredients : ["lum","karso","coconut"] };
+var s1 = {name: "blue hawaii", pic:"blue_hawaii.jpg", abs:"15%", taste: "Watch and see the hawaii", ingredients : ["lum","karso","coconut"] };
 
-var s2 = {name: "screw driver", pic:"empty", abs:"25%", taste: "Dangerous sweetness", ingredients : ["lum","sweet and sour"]};
+var s2 = {name: "blue monday", pic:"blue_monday.jpg", abs:"25%", taste: "Dangerous sweetness", ingredients : ["lum","sweet and sour"]};
 
-var s3 = {name: "blue hawaii", pic:"empty", abs:"15%", taste: "Watch and see the hawaii", ingredients : ["lum","karso","coconut"] };
+var s3 = {name: "blue sapphire", pic:"blue_sapphire.jpg", abs:"15%", taste: "Watch and see the hawaii", ingredients : ["lum","karso","coconut"] };
 
-var s4 = {name: "screw driver", pic:"empty", abs:"25%", taste: "Dangerous sweetness", ingredients : ["lum","sweet and sour"]};
+var s4 = {name: "screw driver", pic:"screw_driver.jpg", abs:"25%", taste: "Dangerous sweetness", ingredients : ["lum","sweet and sour"]};
 
-var s5 = {name: "blue hawaii", pic:"empty", abs:"15%", taste: "Watch and see the hawaii", ingredients : ["lum","karso","coconut"] };
+var s5 = {name: "orange blossom", pic:"orange_blossom.jpg", abs:"15%", taste: "Watch and see the hawaii", ingredients : ["lum","karso","coconut"] };
 
-var s6 = {name: "screw driver", pic:"empty", abs:"25%", taste: "Dangerous sweetness", ingredients : ["lum","sweet and sour"]};
+var s6 = {name: "mimosa", pic:"mimosa.jpg", abs:"25%", taste: "Dangerous sweetness", ingredients : ["lum","sweet and sour"]};
 
-var s7 = {name: "blue hawaii", pic:"empty", abs:"15%", taste: "Watch and see the hawaii", ingredients : ["lum","karso","coconut"] };
 
-var s8 = {name: "screw driver", pic:"empty", abs:"25%", taste: "Dangerous sweetness", ingredients : ["lum","sweet and sour"]};
-
-var interest_cocktails = [s1,s2,s3,s4,s5,s6,s7,s8];
+var interest_cocktails = [s1,s2,s3,s4,s5,s6];
 
 var interestContainer = document.getElementById("interested_items");
 
@@ -22,22 +19,33 @@ var interestContainer = document.getElementById("interested_items");
 
 
 $(document).ready(function() { 
-    bindEvents();
+  
 
 });
 
 
 function addInterestElem(item){
    var itemDiv = document.createElement("div");
-   itemDiv.setAttribute("class","item col-xs-4");
-  itemDiv.innerHTML = "<a>" + item.name;
- 
-  //itemDiv.addEventListener( "click", function(){
-   // this.innerHTML = item.pic;
-    // 
- // }
- // );
+  itemDiv.setAttribute("class","item col-xs-3");
+  
+  
+  var img = document.createElement("img")
+  img.setAttribute("class", "img-responsive interested_img");
+  img.setAttribute("src", "./Images/"+item.pic);
+  itemDiv.appendChild(img)
+
+  itemDiv.innerHTML += ("<br>" + item.name+"<br>");
+  itemDiv.addEventListener( "click", function(){
+    location.href='./index2.html';
+  });
+
   interestContainer.appendChild(itemDiv);
+
+  
+  var marginDiv = document.createElement("div");
+  marginDiv.setAttribute("class", "item col-xs-1");
+
+  interestContainer.appendChild(marginDiv);
 }
 
 function addInterestElems(){
